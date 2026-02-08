158182a7a638f1f37944482e75ce18b22f09f1b4
husnain-278 <husnain327878@gmail.com>
2026-02-08 08:23:35 +0500

    v1 added - removed .env from tracking

--- Carvo/settings.py
+++ Carvo/settings.py
@@ -176,7 +176,7 @@ SENDGRID_SANDBOX_MODE_IN_TEST = False
 
 
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://127.0.0.1:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
@@ -187,7 +187,7 @@ CELERY_TIMEZONE = 'UTC'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': os.getenv('REDIS_CACHE_URL', 'redis://127.0.0.1:6379/1'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
